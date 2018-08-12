fa465b2343c999b215bb9e902d5f83f34a22f4fa
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-08-12 09:52:57 -0500

    task 3

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 
@@ -27,7 +25,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     console.log('Current State is: ' + JSON.stringify(values));
-    alert('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values)); 
   }
 
   render() {
@@ -39,9 +37,8 @@ class CommentForm extends Component {
           <ModalBody>
             <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
               <div className="form-group">
-                <Label>Rating</Label>
-                <Control.select model=".rating" name="rating"
-                  className="form-control">
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select model=".rating" name="rating" id="rating" className="form-control">
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -51,10 +48,10 @@ class CommentForm extends Component {
               </div>
               <div className="form-group">
                 <Label htmlFor="author">Your Name</Label>
-                <Control.text model=".author" name="author" id="author" className="form-control"
+                <Control.text model=".author" name="author" id="author" className="form-control" 
                   validators={{
                     required, minLength: minLength(3), maxLength: maxLength(15)
-                  }} />
+                  }}/>
                 <Errors
                   className="text-danger"
                   model=".author"
@@ -73,7 +70,7 @@ class CommentForm extends Component {
                   }} />
                 <Errors
                   className="text-danger"
-                  model=".comment"
+                  model=".author"
                   show="touched"
                   messages={{
                     required: 'Required'
@@ -88,71 +85,4 @@ class CommentForm extends Component {
   }
 }
 
-function  RenderDish({ dish }) {
-  return (
-    <div className="col-12 col-md-5 m-1">
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
-}
-
-function RenderComments({ comments }) {
-  if (comments) {
-    return (
-      <div className="col-12 col-md-5 m-1">
-        <h4>Comments</h4>
-        <ul className="list-unstyled">
-          {
-            comments.map(comment => {
-              return (
-                <li key={comment.id}>
-                  <p>{comment.comment}</p>
-                  <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date))) }</p>
-                </li>
-              );
-            })
-          }
-        </ul>
-        <CommentForm />
-      </div>);
-  } else {
-    return <div></div>;
-  }
-}
-
-const DishDetail = (props) => {
-  const { dish, comments } = props;
-  
-  if (dish) {
-    return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-          <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{dish.name}</h3>
-          <hr />
-        </div>
-      </div>
-      <div className="row">
-        <RenderDish dish={dish} />
-        <RenderComments comments={comments} />
-      </div>
-    </div>
-    );
-  } else {
-    return (
-      <div></div>
-    );
-  }
-}
-
-export default DishDetail;
+export default CommentForm;
